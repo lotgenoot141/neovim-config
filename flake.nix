05f7e9b0311a9c7b82ed871c4c76ab9cc2c74500
lotgenoot <157558458+lotgenoot141@users.noreply.github.com>
2025-08-19 19:22:11 -0500

    testing

--- flake.nix
+++ flake.nix
@@ -3,90 +3,95 @@
 
     inputs = {
         nixpkgs.url = "github:NixOS/nixpkgs";
-        flake-utils.url = "github:numtide/flake-utils";
     };
 
-    outputs = { self, nixpkgs, flake-utils, ... }:
-        flake-utils.lib.eachDefaultSystem (system: let
-			pkgs = import nixpkgs { inherit system; };
-			plugins = with pkgs.vimPlugins; [
-				catppuccin-nvim
-				cmp-nvim-lsp
-				crates-nvim
-				gitsigns-nvim
-				indent-blankline-nvim
-				lualine-nvim
-				nvim-autopairs
-				nvim-cmp
-				nvim-lspconfig
-				nvim-tree-lua
-				nvim-treesitter
-				nvim-treesitter-context
-				nvim-ufo
-				nvim-web-devicons
-				telescope-nvim
-				cmp-nvim-lsp
-				nvim-cmp
+    outputs = { self, nixpkgs, flake-utils, ... }: {
+		homeModules.default = { config, lib, pkgs, ... }: {
+			options.neovim = {
+				enable = lib.mkEnableOption "Neovim";
+				defaultEditor = lib.mkEnableOption "Neovim as the default editor";
+				vimAlias = lib.mkEnableOption "Symlink vim to nvim binary";
+				languages = {
+					c.enable = lib.mkEnableOption "C";
+					lua.enable = lib.mkEnableOption "Lua";
+					markdown.enable = lib.mkEnableOption "Markdown";
+					nix.enable = lib.mkEnableOption "Nix";
+					python.enable = lib.mkEnableOption "Python";
+					rust.enable = lib.mkEnableOption "Rust";
+					toml.enable = lib.mkEnableOption "TOML";
+					zig.enable = lib.mkEnableOption "Zig";
+					# TODO: other languages
+				};
+				# TODO: colorschemes
+			};
 
-				nvim-treesitter-parsers.c
-				nvim-treesitter-parsers.cpp
-				nvim-treesitter-parsers.lua
-				nvim-treesitter-parsers.markdown
-				nvim-treesitter-parsers.markdown_inline
-				nvim-treesitter-parsers.nix
-				nvim-treesitter-parsers.python
-				nvim-treesitter-parsers.rust
-				nvim-treesitter-parsers.toml
-				nvim-treesitter-parsers.zig
-				
-				# yaml, xml, wgsl, vimdoc, vim, tmux, sway, sql, ron, regex,
-				# latex, json, javascript, javadoc, java, html, go, css, c, asm,
-				# typst
-			];
-			derivation = (pkgs.wrapNeovimUnstable pkgs.neovim-unwrapped) (pkgs.neovimUtils.makeNeovimConfig {
-				customLuaRC = builtins.readFile ./init.lua;
-				plugins = plugins;
-			});
-		in {
-			packages.default = derivation;
-		}) // {
-			homeModules.default = { config, lib, pkgs, ... }: {
-				options.neovim = {
-					enable = lib.mkEnableOption "Neovim";
-					defaultEditor = lib.mkEnableOption "Neovim as the default editor";
-					languages = {
-						c.enable = lib.mkEnableOption "C";
-						lua.enable = lib.mkEnableOption "Lua";
-						markdown.enable = lib.mkEnableOption "Markdown";
-						nix.enable = lib.mkEnableOption "Nix";
-						python.enable = lib.mkEnableOption "Python";
-						rust.enable = lib.mkEnableOption "Rust";
-						toml.enable = lib.mkEnableOption "TOML";
-						zig.enable = lib.mkEnableOption "Zig";
-						# TODO: other languages
-					};
-					# TODO: colorschemes
+			config = lib.mkIf config.neovim.enable {
+				programs.neovim = {
+					enable = true;
+					defaultEditor = config.neovim.defaultEditor;
+					vimAlias = config.neovim.vimAlias;
+					extraLuaConfig = builtins.readFile ./init.lua;
+					extraLuaPackages = [];
+
+					plugins = let
+						plugins = with pkgs.vimPlugins; [
+							catppuccin-nvim
+							cmp-nvim-lsp
+							crates-nvim
+							gitsigns-nvim
+							indent-blankline-nvim
+							lualine-nvim
+							nvim-autopairs
+							nvim-cmp
+							nvim-lspconfig
+							# nvim-tree-lua
+							oil-nvim
+							nvim-treesitter
+							nvim-treesitter-context
+							nvim-ufo
+							nvim-web-devicons
+							telescope-nvim
+							cmp-nvim-lsp
+							nvim-cmp
+						];
+						parsers =
+							with lib;
+							with pkgs.vimPlugins.nvim-treesitter-parsers;
+							with config.neovim.languages;
+						lib.flatten [
+							(lib.optional c.enable [ c cpp ])
+							(lib.optional lua.enable [ lua ])
+							(lib.optional markdown.enable [ markdown markdown_inline ])
+							(lib.optional nix.enable [ nix ])
+							(lib.optional python.enable [ python ])
+							(lib.optional rust.enable [ rust ])
+							(lib.optional toml.enable [ toml ])
+							(lib.optional zig.enable [ zig ])
+							# NOTE: other parsers:
+							# yaml, xml, wgsl, vimdoc, vim, tmux, sway, sql, ron, regex,
+							# latex, json, javascript, javadoc, java, html, go, css, c, asm,
+							# typst
+						];
+					in plugins ++ parsers;
 				};
 
-				config = lib.mkIf config.neovim.enable {
-					home.packages = lib.flatten [
-						self.packages.${pkgs.system}.default
-						pkgs.ripgrep
-						(lib.optional config.neovim.languages.c.enable pkgs.clang-tools)
-						(lib.optional config.neovim.languages.lua.enable pkgs.lua-language-server)
-						(lib.optional config.neovim.languages.markdown.enable pkgs.vscode-langservers-extracted)
-						(lib.optional config.neovim.languages.nix.enable pkgs.nixd)
-						(lib.optional config.neovim.languages.python.enable pkgs.pyright)
-						(lib.optional config.neovim.languages.rust.enable pkgs.rust-analyzer)
-						(lib.optional config.neovim.languages.toml.enable pkgs.taplo)
-						(lib.optional config.neovim.languages.zig.enable pkgs.zls)
-					];
-
-					home.sessionVariables = lib.mkIf config.neovim.defaultEditor {
-						EDITOR = "${self.packages.${pkgs.system}.default}/bin/nvim";
-						VISUAL = "${self.packages.${pkgs.system}.default}/bin/nvim";
-					};
-				};
+				home.packages =
+					with lib;
+					with config.neovim.languages;
+					with pkgs;
+				lib.flatten [
+					pkgs.ripgrep
+					(optional c.enable clang-tools)
+					(optional lua.enable lua-language-server)
+					(optional markdown.enable vscode-langservers-extracted)
+					(optional nix.enable nixd)
+					(optional python.enable pyright)
+					(optional rust.enable rust-analyzer)
+					(optional toml.enable taplo)
+					(optional zig.enable zls)
+				];
 			};
 		};
+	};
 }
+
