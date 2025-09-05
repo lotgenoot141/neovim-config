f05334e7225fb59433878c539c8753e90ca87efe
lotgenoot <157558458+lotgenoot141@users.noreply.github.com>
2025-09-04 20:19:48 -0500

    added more language support

--- flake.nix
+++ flake.nix
@@ -18,6 +18,11 @@
 				vimAlias = lib.mkEnableOption "Symlink vim to nvim binary";
 				languages = {
 					c.enable = lib.mkEnableOption "C";
+					css.enable = lib.mkEnableOption "CSS";
+					html.enable = lib.mkEnableOption "HTML";
+					java.enable = lib.mkEnableOption "Java";
+					javascript.enable = lib.mkEnableOption "JavaScript";
+					json.enable = lib.mkEnableOption "JSON";
 					lua.enable = lib.mkEnableOption "Lua";
 					markdown.enable = lib.mkEnableOption "Markdown";
 					nix.enable = lib.mkEnableOption "Nix";
@@ -37,17 +42,25 @@
 					extraLuaConfig = lib.readFile ./init.lua;
 					package = inputs.neovim-nightly-overlay.packages.${pkgs.system}.default;
 					plugins = with config.neovim; with pkgs.vimPlugins.nvim-treesitter-parsers; lib.flatten [
+						comment
+						vim
 						vimdoc
+
 						(lib.optional languages.c.enable [ c cpp ])
+						(lib.optional languages.css.enable css)
+						(lib.optional languages.html.enable html)
+						(lib.optional languages.java.enable [ java javadoc ])
+						(lib.optional languages.javascript.enable [ javascript jsdoc tsx typescript ])
+						(lib.optional languages.json.enable [ json json5 jsonc ])
 						(lib.optional languages.lua.enable lua)
 						(lib.optional languages.markdown.enable [ markdown markdown_inline ])
 						(lib.optional languages.nix.enable nix)
 						(lib.optional languages.python.enable python)
-						(lib.optional languages.rust.enable rust)
+						(lib.optional languages.rust.enable [ rust ron ])
 						(lib.optional languages.toml.enable toml)
 						(lib.optional languages.typst.enable typst)
-						(lib.optional languages.zig.enable zig)
-						# asm, c, css, go, html, java, javadoc, javascript, json, latex, regex, ron, sql, sway, tmux, vim, wgsl, xml, yaml,
+						(lib.optional languages.zig.enable [ zig zon ])
+						# asm, go, latex, regex, ron, sql, sway, tmux, wgsl, xml, yaml,
 					];
 				};
 
